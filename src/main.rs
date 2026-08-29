bf7706f411dfec44d4169fce3f3000992151f18f
logintimedout <logintimedout980@gmail.com>
2026-08-29 14:00:17 -0700

    Fix typo in comment about learning project

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use winit::{
     window::{Window, WindowBuilder},
 };
 
-//Yea I added excessive comments cause this is mostly a learning project from a toturial I followed
+//Yea I added excessive comments cause this is mostly a learning project from a tutorial I followed
 
 struct State<'window> {
     //Surface/Area of the drawing pallet
